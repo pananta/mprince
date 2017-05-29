eb258339d7428d3cfb6bf6d868a4ec6329ea9024
pananta <saleenatamang12@gmail.com>
2017-05-29 12:25:41 +0545

    ned data addes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-function getCapitalized(a){
-  var cap=a.toUpperCase();
-  return cap;
-}
-var output=getCapitalized("hgijkbijkiuj");
-alert(output);
-function getCube(b){
-  var cube=b*b*b;
-  return cube;
-}
-var output1=getCube(5);
-var output1=getCube(10);
-var output1=getCube(15);
-alert(output1);
-function printTable(n){
-  for (var i=1;i<10;i++){
-    document.write(n+"*"+i+"="+n*i+"</br>");
-  }
-}
+// function getCapitalized(a){
+//   var cap=a.toUpperCase();
+//   return cap;
+// }
+// var output=getCapitalized("hgijkbijkiuj");
+// alert(output);
+// function getCube(b){
+//   var cube=b*b*b;
+//   return cube;
+// }
+// var output1=getCube(5);
+// var output1=getCube(10);
+// var output1=getCube(15);
+// alert(output1);
+// function printTable(n){
+//   for (var i=1;i<10;i++){
+//     document.write(n+"*"+i+"="+n*i+"</br>");
+//   }
+// }
 
 
-for (var i=1;i<10;i++){
-printTable(i);
-}
+// for (var i=1;i<10;i++){
+// printTable(i);
+// }
 
 $.ajax({
 method:'get',
@@ -29,11 +29,15 @@ url:'https://raw.githubusercontent.com/umpirsky/country-list/master/data/ne_NP/c
 success:function(data){
 //alert(data);da
   var parsed=$.parseJSON(data);
-  for(var i=0;i<parsed.length;i++){
-  document.write(parsed[i]);
+  console.log(parsed)
+  for(var a in parsed){
+    document.write(parsed[a]+"</br>");
   }
 },
 error:function(data){
 
 }
 });
+
+setInterval(displayTime,1000);
+alert (displayTime);
